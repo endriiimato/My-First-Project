0428d1d3d453c3c378eb45a2a6031ec511c88e0d
Endri <endrimato9@gmail.com>
2023-10-03 19:31:33 +0200

    first init

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  titleCUstom = 'New Title';
+  test1 = 3
+  test4 = 3
+  test2 = 3
+}
