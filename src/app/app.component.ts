73f12127d5ee0734dd411305c2dab1666c3c316d
Endri <endrimato9@gmail.com>
2023-10-03 19:40:44 +0200

    test per commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   titleCUstom = 'New Title';
   test1 = 3
   test4 = 3
-  test2 = 3
+  
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    this.test1 = 22222222222
+    this.test4 = 333
+  }
 }
+
